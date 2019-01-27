a0bc86dc138d6faac00fde92f512e967ccfda23b
Martin Graversen <martin@graversen.io>
2019-01-27 20:12:00 +0100

    Stand up ElasticSearch client

---
+++ src/main/java/io/graversen/twaddle/config/ElasticsearchConfig.java
@@ -0,0 +1,46 @@
+package io.graversen.twaddle.config;
+
+import org.elasticsearch.client.Client;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Configuration
+@EnableElasticsearchRepositories(basePackages = "io.graversen.twaddle.data.repository.elastic")
+public class ElasticsearchConfig
+{
+    @Value("${elasticsearch.address}")
+    private String elasticAddress;
+    @Value("${elasticsearch.port}")
+    private int elasticPort;
+    @Value("${elasticsearch.cluster-name}")
+    private String elasticClusterName;
+
+    @Bean
+    public Client client()
+    {
+        final TransportAddress transportAddress = new TransportAddress(elasticHost(), elasticPort);
+        final Settings settings = Settings.builder().put("cluster.name", elasticClusterName).build();
+
+        return new PreBuiltTransportClient(settings).addTransportAddress(transportAddress);
+    }
+
+    private InetAddress elasticHost()
+    {
+        try
+        {
+            return InetAddress.getByName(elasticAddress);
+        }
+        catch (UnknownHostException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+}
