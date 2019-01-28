3ecdae718027bcf82582054f9aa210ab75fded75
Martin Graversen <martin@graversen.io>
2019-01-28 21:04:12 +0100

    More test data

--- src/main/java/io/graversen/twaddle/config/ApplicationConfig.java
+++ src/main/java/io/graversen/twaddle/config/ApplicationConfig.java
@@ -1,28 +1,34 @@
 package io.graversen.twaddle.config;
 
-import io.graversen.twaddle.data.Twaddle;
+import io.graversen.twaddle.data.document.Hashtag;
+import io.graversen.twaddle.data.document.Twaddle;
+import io.graversen.twaddle.data.entity.User;
+import io.graversen.twaddle.data.repository.elastic.IHashTagRepository;
 import io.graversen.twaddle.data.repository.elastic.ITwaddleRepository;
+import io.graversen.twaddle.data.repository.jpa.IUserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import java.time.LocalDateTime;
-
 @Configuration
 @ComponentScan(basePackages = {})
+@RequiredArgsConstructor
 public class ApplicationConfig implements ApplicationListener<ApplicationReadyEvent>
 {
     private final ITwaddleRepository twaddleRepository;
-
-    public ApplicationConfig(ITwaddleRepository twaddleRepository)
-    {
-        this.twaddleRepository = twaddleRepository;
-    }
+    private final IHashTagRepository hashTagRepository;
+    private final IUserRepository userRepository;
 
     @Override
     public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent)
     {
-        twaddleRepository.save(new Twaddle("1", "martin-1337", LocalDateTime.now(), "This works!"));
+        twaddleRepository.deleteAll();
+        hashTagRepository.deleteAll();
+
+        hashTagRepository.save(new Hashtag("yolo"))
+        twaddleRepository.save(new Twaddle("martin-1337", "This works!"));
+        userRepository.save(new User("martin-1337", "martin"));
     }
 }
