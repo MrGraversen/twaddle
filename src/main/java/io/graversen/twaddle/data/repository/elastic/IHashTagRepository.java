f7f2c3719490a989cafdd82358a460de8ac810d2
Martin Graversen <martin@graversen.io>
2019-01-28 21:04:48 +0100

    Better lombok usage, clean up documents, etc

--- src/main/java/io/graversen/twaddle/data/repository/elastic/IHashTagRepository.java
+++ src/main/java/io/graversen/twaddle/data/repository/elastic/IHashTagRepository.java
@@ -1,6 +1,6 @@
 package io.graversen.twaddle.data.repository.elastic;
 
-import io.graversen.twaddle.data.Hashtag;
+import io.graversen.twaddle.data.document.Hashtag;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 public interface IHashTagRepository extends ElasticsearchRepository<Hashtag, String>
--- src/main/java/io/graversen/twaddle/data/repository/elastic/ITwaddleRepository.java
+++ src/main/java/io/graversen/twaddle/data/repository/elastic/ITwaddleRepository.java
@@ -1,6 +1,6 @@
 package io.graversen.twaddle.data.repository.elastic;
 
-import io.graversen.twaddle.data.Twaddle;
+import io.graversen.twaddle.data.document.Twaddle;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
