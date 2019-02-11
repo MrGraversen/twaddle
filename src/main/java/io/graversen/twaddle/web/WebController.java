2fe29bd8f4bfa9e76a7cc493ad15d1adcf4d5a6a
Martin Graversen <martin@graversen.io>
2019-02-11 21:03:12 +0100

    Output all users to web

--- src/main/java/io/graversen/twaddle/web/WebController.java
+++ src/main/java/io/graversen/twaddle/web/WebController.java
@@ -1,15 +1,23 @@
 package io.graversen.twaddle.web;
 
+import io.graversen.twaddle.data.repository.jpa.IUserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller("web")
+@RequiredArgsConstructor
 public class WebController
 {
+    private final IUserRepository userRepository;
+
     @GetMapping
     public ModelAndView index()
     {
-        return new ModelAndView("index");
+        ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("users", userRepository.findAll());
+
+        return modelAndView;
     }
 }
