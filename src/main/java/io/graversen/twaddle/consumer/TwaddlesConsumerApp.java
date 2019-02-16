4f8a007accede64438655fb758db12d01a1777e4
Martin Graversen <martin@graversen.io>
2019-02-16 17:09:31 +0100

    Swap thymeleaf for mustache, begin rewrite

--- src/main/java/io/graversen/twaddle/consumer/TwaddlesConsumerApp.java
+++ src/main/java/io/graversen/twaddle/consumer/TwaddlesConsumerApp.java
@@ -11,24 +11,24 @@ import org.springframework.cloud.stream.annotation.Input;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.SubscribableChannel;
 
-@EnableBinding(TwaddlesConsumerApp.Channels.class)
-@SpringBootApplication
-public class TwaddlesConsumerApp
-{
-    public static void main(String[] args)
-    {
-        new SpringApplicationBuilder(TwaddlesConsumerApp.class).web(WebApplicationType.NONE).run(args);
-    }
-
-    @StreamListener("twaddles")
-    public void consumeTwaddle(Twaddle twaddle)
-    {
-        System.out.println(String.format("Received Twaddle: %s", twaddle.getText()));
-    }
-
-    interface Channels
-    {
-        @Input
-        SubscribableChannel twaddles();
-    }
-}
+//@EnableBinding(TwaddlesConsumerApp.Channels.class)
+//@SpringBootApplication
+//public class TwaddlesConsumerApp
+//{
+//    public static void main(String[] args)
+//    {
+//        new SpringApplicationBuilder(TwaddlesConsumerApp.class).web(WebApplicationType.NONE).run(args);
+//    }
+//
+//    @StreamListener("twaddles")
+//    public void consumeTwaddle(Twaddle twaddle)
+//    {
+//        System.out.println(String.format("Received Twaddle: %s", twaddle.getText()));
+//    }
+//
+//    interface Channels
+//    {
+//        @Input
+//        SubscribableChannel twaddles();
+//    }
+//}
