d4d3a2ad0f91cd10465d0a2aba1bfff7bb0ae684
Martin Graversen <martin@graversen.io>
2019-04-07 19:07:35 +0200

    :sparkles: UserService

---
+++ src/main/java/io/graversen/twaddle/service/UserService.java
@@ -0,0 +1,36 @@
+package io.graversen.twaddle.service;
+
+import io.graversen.twaddle.data.entity.User;
+import io.graversen.twaddle.data.repository.jpa.IUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class UserService
+{
+    private final IUserRepository userRepository;
+
+    private List<User> users;
+    private Map<String, User> userCache;
+
+    public List<User> allUsers()
+    {
+        if (users == null || userCache == null)
+        {
+            users = Collections.unmodifiableList(userRepository.findAll());
+            userCache = users.stream().collect(Collectors.toUnmodifiableMap(User::getUserId, user -> user));
+        }
+
+        return users;
+    }
+
+    public Optional<User> getUser(String userId)
+    {
+        Objects.requireNonNullElseGet(userId, Optional::empty);
+        return Optional.ofNullable(userCache.get(userId));
+    }
+}
